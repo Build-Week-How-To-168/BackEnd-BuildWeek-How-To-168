09f8696102954f6be6ff99bb46706ae6f86bc063
cyasmani <yasmanicastaneda@yahoo.com>
2021-01-03 18:03:10 -0800

    Added auth router and user router

--- server.js
+++ server.js
@@ -1,4 +1,26 @@
 const express = require('express')
+const userRouter = require('./users/users-router')
+const session = require('express-session')
+const authRouter = require('./auth/auth-router')
+
+
+const sessionConfig = {
+    name:"monkey",
+    secret:"keep it secret...",
+    cookie: {
+        maxAge: 1000 * 60 * 60,
+        secure: false,
+        httpOnly: true
+    },
+    resave: false,
+    saveUninitialized: false
+}
+
+server.use(express.json());
+server.use(session(sessionConfig))
+server.use("/api/auth", authRouter)
+server.use("/api/users", userRouter)
+
 
 const server = express();
 
