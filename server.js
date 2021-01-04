7f94f5d05fa513d5a85e9fd1ff97d5312d173bb5
cyasmani <yasmanicastaneda@yahoo.com>
2021-01-03 17:57:24 -0800

    Added routers to server

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require('express')
+const userRouter = require('./users/users-router')
+const guideRouter = require('./guides/guides-router')
+const session = require('express-session')
+const authRouter = require('./auth/auth-router')
+const helmet = require("helmet")
+
+const sessionConfig = {
+    name:"monkey",
+    secret:"keep it secret...",
+    cookie: {
+        maxAge: 1000 * 60 * 60,
+        secure: false,
+        httpOnly: true
+    },
+    resave: false,
+    saveUninitialized: false
+}
 
 const server = express();
-
-
+server.use(helmet())
+server.use(express.json());
+server.use(session(sessionConfig))
+server.use("/api/auth", authRouter)
+server.use("/api/users", userRouter)
+server.use("/api/guides", guideRouter)
 server.get('/', (req,res) => {
     res.send("we are up and running")
 })
