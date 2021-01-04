ec8c4ed473d34a1321f0c80a3d964341d6f9b2e6
cyasmani <yasmanicastaneda@yahoo.com>
2021-01-03 18:49:48 -0800

    Commented out all auth

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 const express = require('express')
 const userRouter = require('./users/users-router')
-const session = require('express-session')
-const authRouter = require('./auth/auth-router')
+//const session = require('express-session')
+//const authRouter = require('./auth/auth-router')
 const server = express();
 
 
-const sessionConfig = {
-    name:"monkey",
-    secret:"keep it secret...",
-    cookie: {
-        maxAge: 1000 * 60 * 60,
-        secure: false,
-        httpOnly: true
-    },
-    resave: false,
-    saveUninitialized: false
-}
+// const sessionConfig = {
+//     name:"monkey",
+//     secret:"keep it secret...",
+//     cookie: {
+//         maxAge: 1000 * 60 * 60,
+//         secure: false,
+//         httpOnly: true
+//     },
+//     resave: false,
+//     saveUninitialized: false
+// }
 
 
 server.use(express.json());
-server.use(session(sessionConfig))
-server.use("/api/auth", authRouter)
+//server.use(session(sessionConfig))
+//server.use("/api/auth", authRouter)
 server.use("/api/users", userRouter)
 
 
--- users/users-router.js
+++ users/users-router.js
@@ -1,6 +1,6 @@
 const router =  require('express').Router()
 const Users = require('./users-model')
-const restricted = require('../auth/restricted-middleware')
+//const restricted = require('../auth/restricted-middleware')
 
 router.get("/",  (req, res) => {
     Users.find()
