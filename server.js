c9dc10f56044ebd2c7d0d8baadb8263781f43b10
cyasmani <yasmanicastaneda@yahoo.com>
2021-01-03 19:23:18 -0800

    added helmet and guides

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const guideRouter = require('./guides/guides-router')
 const server = express();
 
-
+server.use(helmet())
+server.use("/api/guides", guideRouter)
 server.get('/', (req,res) => {
     res.send("we are up and running")
 })
