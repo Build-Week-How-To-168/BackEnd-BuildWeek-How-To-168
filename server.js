472255a64749e2c8843e920f750c5a7a755dcd8a
cyasmani <yasmanicastaneda@yahoo.com>
2021-01-03 19:25:11 -0800

    removed guides

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const guideRouter = require('./guides/guides-router')
 const server = express();
 
 server.use(helmet())
-server.use("/api/guides", guideRouter)
+
 server.get('/', (req,res) => {
     res.send("we are up and running")
 })
